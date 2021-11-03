512df5539595a45335f342ef732fdc5199213515
KosukeKorokke12 <93626983+KosukeKorokke12@users.noreply.github.com>
2021-11-03 13:15:55 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,17 +2,19 @@
 const CACHE_NAME = 'pwa-sample-caches-v1';
 // Cache targets
 const urlsToCache = [
-    '/Suuziwari/',
-    '/Suuziwari/BGM.mp3',
-    '/Suuziwari/dont%20touch/tyuui.html',
-    '/Suuziwari/dont%20touch/korokke.js',
-    '/Suuziwari/dont%20touch/jquery-3.6.0.min.js',
-    '/Suuziwari/dont%20touch/ress.min.js',
-    '/Suuziwari/dont%20touch/style.css',
-    '/Suuziwari/dont%20touch/Answer.mp3',
-    '/Suuziwari/dont%20touch/buzzer.mp3',
-    '/Suuziwari/dont%20touch/suuziwari.png',
-    '/Suuziwari/dont%20touch/Suuziwari.png',
+    './Suuziwari/',
+    './Suuziwari/index.html',
+    './Suuziwari/index.min.html',
+    './Suuziwari/BGM.mp3',
+    './Suuziwari/dont%20touch/tyuui.html',
+    './Suuziwari/dont%20touch/korokke.js',
+    './Suuziwari/dont%20touch/jquery-3.6.0.min.js',
+    './Suuziwari/dont%20touch/ress.min.js',
+    './Suuziwari/dont%20touch/style.css',
+    './Suuziwari/dont%20touch/Answer.mp3',
+    './Suuziwari/dont%20touch/buzzer.mp3',
+    './Suuziwari/dont%20touch/suuziwari.png',
+    './Suuziwari/dont%20touch/Suuziwari.png',
 ];
 
 self.addEventListener('install', (event) => {
