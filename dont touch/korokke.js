bff36d47d816e7426863c475bc7ec0a2da8a20cf
KosukeKorokke12 <93626983+KosukeKorokke12@users.noreply.github.com>
2021-11-14 20:20:18 +0900

    Update korokke.js

--- dont touch/korokke.js
+++ dont touch/korokke.js
@@ -528,6 +528,7 @@ if (e.KeyCode == 112 || e.KeyCode == 113 || e.KeyCode == 114 || e.KeyCode == 115
 	if (e.code === 'Space') {
 		event.preventDefault();
 		event.returnValue = false;
+		scrollTo(0, 50);
 		scrollTo(0, 0);
 		if (started.textContent == 0) {
 		scrollTo(0, 0);
